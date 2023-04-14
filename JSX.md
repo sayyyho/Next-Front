39323a4ee889cbc30d27b16a21c41bb5c9c62d8e
sayyyho <323psh@naver.com>
2023-04-14 23:48:48 +0900

    page.js에서 JSX 리마인드

---
+++ JSX.md
@@ -0,0 +1,6 @@
+# JSX
+
+- return 아래 하나의 태그만
+- class 대신 className
+- {}에 js 변수를 넣을 수 있음
+- JSX 내에서 style 변경은 {{ color : "red" }}처럼 style 변경
--- app/page.js
+++ app/page.js
@@ -1,3 +1,15 @@
 export default function Home() {
-  return <div></div>;
+  let name = "sehopark";
+  const site = "https://www.sayyyho.site";
+  return (
+    <div>
+      <h2 style={{ color: "black", background: "white" }} className="title">
+        SEHO PARK
+      </h2>
+      <a href={site}>view my page</a>
+      <footer>
+        <p className="sub">by dev {name}</p>
+      </footer>
+    </div>
+  );
 }
